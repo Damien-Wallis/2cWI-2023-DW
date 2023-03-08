071537d7a44a4f5133bbe20e515926506cbf03fb
Damien Wallis <damie.grobi@gmail.com>
2023-03-08 09:03:39 +0100

    01_Variablen_Operationen_finished

---
+++ 01_Variablen_Operationen/script.js
@@ -0,0 +1,42 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+
+let result = a + b + c;
+console.log("a + b + c = " + result);
+
+result = (a + b) / c;
+console.log("(a + b) /c = " + result);
+
+result = c - a + b;
+console.log("c - a + b = " + result);
+
+result = a + b + c * 3;
+console.log("a + b + c * 3 = " + result);
+
+result = (a + b + c) / 4;
+console.log("(a + b + c) / 4 = " + result); 
+
+//Rechnungen mit veränderten Variablen
+a = 3;
+b = a + 3;
+c = c - 3;
+
+result = a + b + c;
+console.log("a + b + c = " + result);
+
+result = (a + b) / c;
+console.log("(a + b) /c = " + result);
+
+result = c - a + b;
+console.log("c - a + b = " + result);
+
+result = a + b + c * 3;
+console.log("a + b + c * 3 = " + result);
+
+result = (a + b + c) / 4;
+console.log("(a + b + c) / 4 = " + result);
