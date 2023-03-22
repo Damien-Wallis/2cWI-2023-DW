a1df1456d09bb2c7ece5b331c7a77c0858d0e0aa
Damien Wallis <damie.grobi@gmail.com>
2023-03-22 09:32:06 +0100

    04_finished

---
+++ 04_Schleifen/script.js
@@ -0,0 +1,29 @@
+for (let count1 = 7;count1<=45;count1++) {
+    console.log(count1);
+}
+
+for (let count2 = 9;count2<=99;count2++) {
+    console.log(count2);
+}
+
+for (let count3 = 10;count3>=0;count3--) {
+    console.log(count3);
+}
+
+for (let count4 = 1234;count4>=1207;count4--) {
+    console.log(count4);
+}
+
+for (let count5 = 1;count5<=8;count5++) {
+    console.log("Damien");
+}
+
+for (let count6 = 0;count6<=10;count6++) {
+    console.log(count6);
+}
+
+console.log("Damien");
+
+for (let count7 = 10;count7>=0;count7--) {
+    console.log(count7);
+}
