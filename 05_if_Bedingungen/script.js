48903f5d8be9b38c3557fa9b2e3e9dec00d5419c
Damien Wallis <damie.grobi@gmail.com>
2023-04-09 23:44:31 +0200

    05_finished

---
+++ 05_if_Bedingungen/script.js
@@ -0,0 +1,35 @@
+let number = 86;
+
+//Aufgabe 1
+
+//kleiner als 20 = "Mini"
+if (number < 20) {
+    console.log("Mini");
+}
+
+//zwischen 20 und 50 = "Medium"
+else if (number > 20 && number < 50) {
+    console.log("Medium");
+}
+
+//größer als 50 = "Large"
+else if (number > 50) {
+    console.log("Large");
+}
+
+//Aufgabe 2
+
+//kleiner gleich 7 = "Mini"
+if (number <= 7) {
+    console.log("Mini");
+}
+
+//zwischen 7 und 90 = "Medium"
+else if (number > 7 && number < 90) {
+    console.log("Medium");
+}
+
+//größer als 90 = "Large"
+else if (number > 90) {
+    console.log("Large");
+}
