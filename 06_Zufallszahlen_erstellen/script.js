746f49e34c24009b5db03a8bdcd4a6ac62577a71
Damien Wallis <damie.grobi@gmail.com>
2023-04-19 17:01:09 +0200

    06_finished

---
+++ 06_Zufallszahlen_erstellen/script.js
@@ -0,0 +1,13 @@
+//Zufallszahlen
+let randomNumber = Math.random();
+console.log("Eine Zufallszahl: " + randomNumber);
+
+//Zufallszahlen zwischen 0 und 100 ohne Nachkommastellen
+let randomNumber2 = Math.random() * 100;
+let random100 = Math.floor(randomNumber2);
+console.log("Eine Zufallszahl zwischen 0 und 100 ohne Nachkommastellen: " + random100);
+
+//Zufallszahlen von 11-20
+let randomNumber3 = Math.random() * 10 + 10;
+let random20 = Math.floor(randomNumber3);
+console.log("Eine Zufallszahl von 11-20 :" + random20);
