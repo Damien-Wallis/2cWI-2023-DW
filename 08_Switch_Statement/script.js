b9b2fae0498cfea688c7280aa5dd0dece26bac9a
Damien Wallis <damie.grobi@gmail.com>
2023-04-26 09:11:40 +0200

    08_finished

---
+++ 08_Switch_Statement/script.js
@@ -0,0 +1,22 @@
+let randomNumber = Math.floor(Math.random() * 6) + 5;
+
+switch(randomNumber) {
+    case 10:
+        console.log("Ten");
+        break;
+
+    case 9:
+        console.log("Nine");
+        
+    case 8:
+        console.log("Eight");
+    
+    case 7:
+        console.log("Seven");
+
+    case 6:
+        console.log("Six");
+
+    case 5:
+        console.log("Five");
+}
