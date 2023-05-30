5f864c3de471969eda747842c6e4f45815d607e6
Damien Wallis <damie.grobi@gmail.com>
2023-05-30 18:43:14 +0200

    11_Aufgabe2

--- 11_Arrays/script.js
+++ 11_Arrays/script.js
@@ -12,3 +12,9 @@ console.log("Summe: " + Summe);
 
 let Mittelwert = Summe / arr.length
 console.log("Mittelwert: " + Mittelwert);
+
+
+let Freunde = ["Susi", "Paula", "Hans"];
+Freunde.push("Sepp");
+
+console.log("Meine Freunde sind " + Freunde[0] + ", " + Freunde[1] + ", " + Freunde[2] + " und " + Freunde[3]);
